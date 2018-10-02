7d019659ca83191f61a3492bffe7dbf38d51c4b4
diurno <diurno@pc-aula>
2018-10-02 13:05:55 +0200

    ejercicios

---
+++ src/Ejercicio1.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=0,num2=0,num3=0,num4=0,num5=0;
+		
+		num1=Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero: "));
+		num2=Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero: "));
+		num3=Integer.parseInt(JOptionPane.showInputDialog("Introduce el tercer numero: "));
+		num4=Integer.parseInt(JOptionPane.showInputDialog("Introduce el cuarto numero: "));
+		num5=Integer.parseInt(JOptionPane.showInputDialog("Introduce el quinto numero: "));
+		
+		
+		
+
+	}
+
+}
