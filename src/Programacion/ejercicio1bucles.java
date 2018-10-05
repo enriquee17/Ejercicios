75ff7325b2eeb8936846c07a062cefdfb586f2b7
diurno <diurno@pc-aula>
2018-10-05 14:58:40 +0200

    dfgdgdgdg

--- src/Programacion/ejercicio1bucles.java
+++ src/Programacion/ejercicio1bucles.java
@@ -7,18 +7,30 @@ public class ejercicio1bucles {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		
-		int i, acumuladorNumerosMayores10 = 0;
-		
-		for (i = 1; i < 5; i++) {
-			int numero=Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero " + i));
-			
-			
-		if (numero>10) {
-		acumuladorNumerosMayores10 += numero;
-			}
-			
-		}
-		JOptionPane.showMessageDialog(null, acumuladorNumerosMayores10, , messageType);
+		// Declaraci�n de las variables
+				int i, acumuladorNumerosMayoresDiez = 0;
+				int cantidadNumerosAPedir = 0;
+				
+				// Petici�n al usuario de la cantidad de n�meros
+				cantidadNumerosAPedir = Integer.parseInt(
+						JOptionPane.showInputDialog("Introduzca cu�ntos n�meros desea"));
+				
+				// Bucle for que se ejecuta n veces
+				for (i = 0; i < cantidadNumerosAPedir; i++) {
+					// Petici�n del n�mero al usuario
+					int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca n�mero " + (i+1)));
+					
+					// Si el n�mero no es mayor a 10 no lo tenemos en cuenta
+					if (numero > 10) {
+						// Si el n�mero es mayor a 10 entramos al condicional
+						// y se acumula el valor
+						acumuladorNumerosMayoresDiez += numero;
+					}
+				}
+				
+				// Sacamos el mensaje final con el valor acumulado
+				JOptionPane.showMessageDialog(null, "Total acumulado " + acumuladorNumerosMayoresDiez);
+
 	}
 
 }
